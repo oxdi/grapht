6f9b13277f4b2ac06cea9555ba0bb0a6949ae08e
Chris Farmiloe <chris@oxdi.eu>
2016-09-12 14:42:33 +0000

    xvfb headless

--- src/js/grapht/test/runner.sh
+++ src/js/grapht/test/runner.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-(cd ../../../ && echo '' > data/jstest && GOPATH=$(pwd) go build -o bin/grapht grapht && ./bin/grapht &) && ./node_modules/.bin/browserify test/connection_test.js| ./node_modules/.bin/tape-run | ./node_modules/.bin/colortape; kill `ps aux | grep bin/grapht | awk '{print $2}'`
+(
+	cd ../../../ &&
+	echo '' > data/jstest &&
+	./bin/grapht >/dev/null &
+) && xvfb-run npm test
+
+kill `ps aux | grep bin/grapht | awk '{print $2}'` 2>/dev/null >/dev/null
